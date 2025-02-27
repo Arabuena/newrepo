a3ae740a5770c2b078e61710b22c0eb9ff474448
Arah <ara100limite@gmail.com>
2025-02-27 11:13:38 -0300

    Corrigindo variáveis para build do frontend

---
+++ src/components/RideChat.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+
+const RideChat = () => {
+  const fetchMessages = () => {
+    // Implementation of fetchMessages
+  };
+
+  useEffect(() => {
+    fetchMessages();
+  }, [fetchMessages]);
+
+  return (
+    // Rest of the component code
+  );
+};
+
+export default RideChat; 
---
+++ src/components/SupportChat.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+
+const SupportChat = () => {
+  const loadMessages = () => {
+    // Implementation of loadMessages function
+  };
+
+  useEffect(() => {
+    loadMessages();
+  }, [loadMessages]);
+
+  return (
+    // Rest of the component code
+  );
+};
+
+export default SupportChat; 
